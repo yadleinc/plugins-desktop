31765cc6c750631331206ecdffaeed9f834246a6
vmlaker <velimir.mlaker@gmail.com>
2019-11-13 15:25:10 -0800

    Reuse var.

--- python/bucket.py
+++ python/bucket.py
@@ -14,8 +14,9 @@ sys.path.append(os.path.dirname(sys.executable))
 from log_config import log_config
 logging.config.dictConfig(log_config)
 
-home_dir = os.path.expanduser('~')
+
 file_path = sys.argv[1]
+home_dir = os.path.expanduser('~')
 default_bucket = os.path.join(home_dir, 'Desktop', 'bucket_1')
 icon = os.path.join(home_dir, 'yadle', 'plugins', 'logo.png')
 bucket_data = os.path.join(home_dir, 'yadle', 'plugins', 'bucket1.data')
@@ -23,10 +24,10 @@ bucket_data = os.path.join(home_dir, 'yadle', 'plugins', 'bucket1.data')
 if not os.path.isfile(bucket_data):
 	logging.debug('{} does not exist, creating'.format(bucket_data))
 	with open(bucket_data, 'w') as f:
-		f.write(default_bucket)
+		f.write(default_bucket + '\n')
 
 logging.debug('Reading bucket location from {}'.format(bucket_data))
-with open(os.path.join(home_dir, 'yadle', 'plugins', 'bucket1.data')) as f:
+with open(bucket_data) as f:
 	bucket = f.read().strip()
 
 logging.debug('file_path: {}'.format(file_path))
