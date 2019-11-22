5f1c9cccf123b252cd9d9cb63ae39dd0fdf633a7
vmlaker <velimir.mlaker@gmail.com>
2019-11-22 14:05:41 -0800

    Add argument types.

--- python/test_api.py
+++ python/test_api.py
@@ -10,19 +10,16 @@ import requests
 import log_config
 
 
-file_path = sys.argv[1]
-server_url = sys.argv[2]
-app_id = sys.argv[3]
-token_key = sys.argv[4]
-device_id = sys.argv[5]
-file_id = sys.argv[6]
+server_url = sys.argv[1]
+app_id = sys.argv[2]
+token_key = sys.argv[3]
+file_ids = sys.argv[4:]
 
-logging.debug('file_path  : {}'.format(file_path))
 logging.debug('server_url : {}'.format(server_url))
 logging.debug('app_id     : {}'.format(app_id))
 logging.debug('token_key  : {}'.format(token_key))
-logging.debug('device_id  : {}'.format(device_id))
-logging.debug('file_id    : {}'.format(file_id))
+for file_id in file_ids:
+    logging.debug('file_id    : {}'.format(file_id))
 
 headers = {
     'x-app-id': app_id,
@@ -31,7 +28,8 @@ headers = {
     'Content-Type': 'application/json',
 }
 
-url = '{}/yadle/v2/file/{}'.format(server_url.rstrip('/'), file_id)
-response = requests.get(url, headers=headers)
-response = json.loads(response.text)
-logging.debug(response)
+for file_id in file_ids:
+    url = '{}/yadle/v2/file/{}'.format(server_url.rstrip('/'), file_id)
+    response = requests.get(url, headers=headers)
+    response = json.loads(response.text)
+    logging.debug(response)
