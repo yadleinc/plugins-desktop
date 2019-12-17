76057e51aba9d30711c26c252f9011d5632c7894
vmlaker <velimir.mlaker@gmail.com>
2019-12-16 17:43:52 -0800

    Fix bucket plugin to work in bulk mode.

--- python/bucket.py
+++ python/bucket.py
@@ -1,4 +1,4 @@
-""" Plugin for the Yadle desktop app that copies the file to a folder.
+""" Plugin for the Yadle desktop app that copies file(s) to a folder.
 """
 
 import logging
@@ -10,7 +10,7 @@ import sys
 import log_config
 
 
-file_path = sys.argv[1]
+file_paths = sys.argv[1:]
 home_dir = os.path.expanduser('~')
 default_bucket = os.path.join(home_dir, 'Desktop', 'bucket_1')
 icon = os.path.join(home_dir, 'yadle', 'plugins', 'logo.png')
@@ -25,7 +25,7 @@ logging.debug('Reading bucket location from {}'.format(bucket_data))
 with open(bucket_data) as f:
     bucket = f.read().strip()
 
-logging.debug('file_path: {}'.format(file_path))
+logging.debug('file_paths: {}'.format(file_paths))
 logging.debug('home_dir: {}'.format(home_dir))
 logging.debug('default_bucket: {}'.format(default_bucket))
 
@@ -33,15 +33,12 @@ if not os.path.isdir(bucket):
     logging.debug('Creating bucket {}'.format(bucket))
     os.makedirs(bucket)
 
-logging.debug('Copying {} to {}'.format(file_path, bucket))
-try:
-    shutil.copy(file_path, bucket)
-except:
-    logging.error('Failed to copy {} to {}'.format(file_path, bucket))
-    sys.exit(1)
-
-logging.debug('Notifying')
-command = "notify-send -t 5000 -u normal -i {} 'Copied {} to {}'".format(
-    icon, os.path.basename(file_path), bucket)
-call(command, shell=True)
-logging.debug('Notified')
+for file_path in file_paths:
+    logging.debug('Copying {} to {}'.format(file_path, bucket))
+    try:
+        shutil.copy(file_path, bucket)
+        command = "notify-send -t 5000 -u normal -i {} 'Copied {} to {}'".format(
+            icon, os.path.basename(file_path), bucket)
+        call(command, shell=True)
+    except:
+        logging.error('Failed to copy {} to {}'.format(file_path, bucket))
