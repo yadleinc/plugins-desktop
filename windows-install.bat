af17cf5e329637bc83c112d283392ad7982c44f4
andrew <andrew@yadle.com>
2020-04-21 12:06:59 -0700

    updated windows installer

--- windows-install.bat
+++ windows-install.bat
@@ -1,8 +1,8 @@
+@echo off
+
 rem this program takes the plugins from the 'windows' folder
 rem and stores them in %userprofile%\yadle\plugins
 rem for the yadle desktop app to access
-
-@echo off
 
 rem Display YADLE logo
 echo          __       __
@@ -12,13 +12,18 @@ echo --------------------
 
 set newfolder="%userprofile%\yadle\plugins"
 
+rem check for newfolder
 IF NOT EXIST %newfolder% (
    mkdir %newfolder%
 )
 
+rem copy plugins
+echo Installing plugins...
 copy plugins.json %newfolder%
 copy windows\* %newfolder%
+echo Plugins installed
+echo
 
-echo "Done, press any key to close."
+echo Press any key to close this window
 pause
 
